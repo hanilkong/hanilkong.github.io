8849b7ad019d6ab31a962dfed9a5d1a31358565a
hanilkong <35002119+hanilkong@users.noreply.github.com>
2018-04-09 18:58:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,18 @@ if ('serviceWorker' in navigator) {
     console.log('서비스워커 등록 실패: ', err);
   });
 }
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cache-name').then(function(cache) {
+      return cache.addAll([
+        '/blog/',
+        '/blog/assets/js/scripts.min.js',
+        '/blog/images/nhnent.png'
+           ]);
+    }).then(function(){
+      console.log('설치완료');
+    }).catch(function(){
+      console.log('설치실패');
+    })
+  );
+});
