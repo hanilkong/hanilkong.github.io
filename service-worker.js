f84fdda2f1cbf76ef6d0f6dfd88bc055128a2792
hanilkong <35002119+hanilkong@users.noreply.github.com>
2018-04-09 18:53:22 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/blog/sw.js', {
+    scope: '/blog/'
+  }).then(function(registration) {
+    console.log('서비스워커 등록 성공 ', registration.scope);
+  }).catch(function(err) {
+    console.log('서비스워커 등록 실패: ', err);
+  });
+}
